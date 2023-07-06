31e8931fa0a5c4c99777b1b83b9767fe382c4c54
kabinny <kabinny@gmail.com>
2023-07-06 23:49:15 +0900

    Definitely Typed

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import crypto from 'crypto'
+
 interface BlockShape {
   hash: string
   prevHash: string
@@ -16,5 +18,6 @@ class Block implements BlockShape {
   }
   static calculateHash(prevHash: string, height: number, data: string) {
     const toHash = `${prevHash}${height}${data}`
+    return crypto.createHash('sha256').update(toHash).digest('hex')
   }
 }
